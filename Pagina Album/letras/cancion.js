0cd0fe1cbd7556980d46c412ef4438d23efaa531
FranDure <fdure8579@gmail.com>
2024-06-22 21:17:32 -0300

    "archivos nuevos"

---
+++ Pagina Album/letras/cancion.js
@@ -0,0 +1,23 @@
+function getParameterByName(name) {
+    const url = new URL(window.location.href);
+    return url.searchParams.get(name);
+} // esto toma los datos en base al ID que se elige en la URL
+
+const songId = getParameterByName('id') || '1'; // crea un ID en base al numero que se muestre en la misma URL
+//o toma el primero por defecto
+
+if (songId && canciones[songId]) {
+    const song = canciones[songId];
+    document.getElementById('letra-cancion').textContent = song.letra;
+    document.getElementById('titulo-cancion').textContent = song.titulo;
+    document.getElementById('artista-cancion').textContent = song.artista;
+    document.getElementById('year-cancion').textContent = song.year;
+    document.getElementById('album-cancion').textContent = song.album;
+
+} else {
+    document.getElementById('letra-cancion').textContent = "error";
+    document.getElementById('titulo-cancion').textContent = "error";
+    document.getElementById('artista-cancion').textContent = "error";
+    document.getElementById('year-cancion').textContent = "error";
+    document.getElementById('album-cancion').textContent = "error";
+}
---
+++ Pagina Album/letras/canciones.js
@@ -0,0 +1,517 @@
+const canciones = {
+    1 : {
+        titulo : `desconocido`,
+        artista : `artista desconocido`,
+        album : `desconocido`,
+        year : `2002`,
+        letra : 
+        `Aqui no hay nada.`,
+    } ,
+
+    101: {
+        titulo: `RUNNING OUT OF TIME`, 
+        artista : `Tyler, The Creator`,
+        album : `IGOR`,
+        year : `2019`,
+        letra: `
+Runnin' out of time, runnin' out of time, runnin' out of time
+Runnin' out of time to make you love me
+Runnin' out of time, runnin' out of time
+To make you love me (secrets)
+Oh, you spin my head around (oh, you spin my head around)
+I've been lookin' for it (for it, for it)
+I've been runnin' out of spells
+To make you love me
+(Secrets)
+Keep it up
+To make you love me
+I wade in your water (wade in your water)
+Your waves wash over me
+I drift to the deep end
+Don't save, don't save, don't save
+It's a low tide (I'll be fine)
+I found peace in drownin'
+Runnin' out of time, runnin out of time, runnin' out of time
+Runnin' out of time, to make you love me
+Livin' in pretend
+Keep it a buck fifty fuck
+Are you livin' in pretend?
+Are you-
+So take your mask off
+I need her out the picture (wade in your water)
+Take your mask off (yeah)
+Stop lyin' for these niggas
+Stop lyin' to yourself
+I know the real you
+(Don't save, don't save, don't save)
+Halloween ain't for a minute, lose the costume
+You need to chill, okay
+We runnin' from the targets and 'em back in the day
+And now they workin' their all, another track at the DAW
+Actin' a fool, actin' a ball, we packin' the park, yeah
+But I'm still runnin'
+Out of time
+runnin' outta
+(Of time) still runnin' outta
+Of time
+Still runnin' outta time (time)
+Yea`
+    },
+
+    102 : { 
+        titulo :  `Tabú`,
+        artista : `Gustavo Cerati`,
+        album : `Bocanada`,
+        year : `1999`,
+        letra : `
+Cerca del nuevo fin
+Tabú, fuego y dolor
+La selva se abrió a mis pies
+Y por ti, tuve el valor
+De seguir
+Al ánimo de brillar
+La luz se adelantó
+Trepando montañas fui
+Y perdí la tentación
+Por ti
+Lo hice por ti
+Por ti
+Tuve el valor
+Corrí hasta alcanzarte
+Y vencí la sórdida sed
+En la alquimia salvaje
+De tus labios oro rubí
+Por ti
+Lo hice por ti
+Por ti
+Tuve el valor`
+    },
+
+    
+    2101 : {
+        titulo :  `MALAMENTE (Cap.1: Augurio)`,
+        artista : `Rosalía`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Ese cristalito roto
+Yo sentí como crujía
+Antes de caerse al suelo
+Ya sabía que se rompía (uh)
+Está parpadeando
+La luz del descansillo
+Una voz en la escalera
+Alguien cruzando el pasillo
+Malamente (eso es; así si)
+Malamente (¡tra, tra!)
+Mal, mu' mal, mu' mal, mu' mal, mu' mal (mira)
+Malamente (toma que toma)
+Está en la mente (eso es; ¡illo!)
+Malamente
+Mal, mu' mal, mu' mal, mu' mal, mu' mal
+Malamente (uh)
+Se ha puesto la noche rara
+Han sali'o luna' y estrella' (eh)
+Me lo dijo esa gitana (¿qué?)
+Mejor no salir a verla (no)
+Sueño que estoy andando
+Por un puente y que la acera (mira, mira, mira, mira)
+Cuanto más quiero cruzarlo (va)
+Más se mueve y tambalea
+Malamente (eso es; así si)
+Malamente (¡tra, tra!)
+Mal, mu' mal, mu' mal, mu' mal, mu' mal (mira)
+Malamente (toma que toma)
+Está en la mente (eso es; ¡illo!)
+Malamente
+Mal, mu' mal, mu' mal, mu' mal, mu' mal
+Malamente
+Aunque no esté bonita
+La noche undivé
+Voy a salir pa la calle
+En la manita los aro' brillando, en mi piel los corale'
+Me protejan, me salven
+Me iluminen, me guarden
+Y por delante
+No voy a perder ni un minuto en volver a pensarte
+Malamente (eso es; así si)
+Malamente (¡tra, tra!)
+Mal, mu' mal, mu' mal, mu' mal, mu' mal (mira)
+Malamente (toma que toma)
+Está en la mente (eso es; ¡illo!)
+Malamente
+Mal, mu' mal, mu' mal, mu' mal, mu' mal (illo)
+Malamente (toma que toma)
+Está en la mente (¡illo!)
+Malamente (¡tra, tra!)
+Mal, mu' mal, mu' mal, mu' mal, mu' mal
+Malamente (toma que toma)
+Está en la mente (eso es; ¡illo!)
+Malamente
+Mal, mu' mal, mu' mal, mu' mal, mu' mal
+Malamente`
+    },
+
+    2102 : { 
+        titulo :  `QUE NO SALGA LA LUNA (Cap.2: Boda)`,
+        artista : `Rosalía`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Qué suerte la que yo tuve
+El día que lo encontré
+Señalaito a punta de navaja
+Prima, sobre la pared
+Señalaito a punta de navaja
+Prima, sobre la pared
+Como las hojas de un cuchillo
+Brillaban los sacáis suyos cuando le di el anillo
+(Brillaban los sacáis, brillaban los sacáis)
+Brillaban los sacáis suyos
+(Brillaban los sacáis, brillaban los sacáis)
+Que brillanan sus sacáis (brillaban los sacáis)
+Si hay alguien que aquí se oponga
+Que no levante la voz (que no lo escuche la novia)
+(Que no salga la luna, que no tiene pa' qué)
+(No tiene pa' qué, no tiene pa' qué)
+Con tus ojitos, prima, yo me alumbraré (que no tiene pa' qué)
+(No tiene pa' qué, no tiene pa' qué)
+Con tus ojitos, prima, yo me alumbraré (que no tiene pa' qué)
+(No tiene pa' qué, no tiene pa' qué)
+Con tus ojitos, prima, yo me alumbraré
+A ver, a ver, a ver; enséñame ese
+¡Como brilla! ¡Madre mía, qué guapo!
+Diamantes, ahora sí que con diamantes, me gusta
+Qué bonita está mi novia, que se pare en su trono (reina)
+Corona' de brillantes y, ay, con perlas y oro
+Corona' de brillantes y, ay, con perlas y oro
+(Quiera o no quiera, no quiera, ella no quiera)
+(Va estar conmigo y hasta que se muera)
+Clava de plata (wooh)
+Clava de plata
+Sin decir na', a mí me ha jurao que ella por mí se mata
+Sin decir na', a mí me ha jurao que ella por mí se mata
+A la virgencita de la Merced, un rezo
+La hoguerita se apagó que por sus besos
+La hoguerita se apagó que por sus besos
+Si hay alguien que aquí se oponga
+Que no levante la voz (que no lo escuche la novia)
+(Que no salga la luna, que no tiene pa' qué)
+(No tiene pa' qué, no tiene pa' qué)
+Con tus ojitos, prima, yo me alumbraré (que no tiene pa' qué)
+(No tiene pa' qué, no tiene pa' qué)
+Que en tus ojitos, prima, yo me alumbraré (que no tiene pa' qué)
+(No tiene pa' qué, no tiene pa' qué)
+Con tus ojitos, prima, yo me alumbraré`
+    },
+    
+    2103 : { 
+        titulo :  `Pienso en tu mirá (Cap.3: celos)`,
+        artista : `Rosalía`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Me da miedo cuando sales
+Sonriendo pa' la calle
+Porque todos pueden ver
+Los hoyuelitos que te salen
+Y del aire cuando pasa
+Por levantarte el cabello
+Y del oro que te viste
+Por amarrarse a tu cuello
+Y del cielo y de la luna
+Porque tú quieras mirarlo
+Hasta del agua que bebes
+Cuando te mojas los labios
+Pienso en tu mirá, tu mirá clavá es una bala en el pecho
+Pienso en tu mirá, tu mirá clavá es una bala en el pecho
+Pienso en tu mirá, tu mirá clavá es una bala en el pecho
+Pienso en tu mirá
+Pienso en tu mirá
+Me da miedo cuando sales
+Sonriendo pa' la calle
+Porque todos pueden ver
+Los hoyuelitos que te salen
+Me da miedo cuando sales
+Sonriendo pa' la calle
+Porque todos pueden ver
+Los hoyuelitos que te salen
+Tan bonita que amenaza
+Cuando callas me das miedo
+Tan fría como la nieve
+Cuando cae desde el cielo
+Cuando sales por la puerta
+Pienso que no vuelves nunca
+Y si no te agarro fuerte
+Siento que será mi culpa
+Pienso en tu mirá, tu mirá clavá es una bala en el pecho
+Pienso en tu mirá, tu mirá clavá es una bala en el pe'
+Pienso en tu mirá, tu mirá clavá es una bala en el pecho
+Pienso en tu mirá, tu mirá clavá es una bala en el pe'
+Pienso en tu mirá, tu mirá clavá es una bala en el pecho
+Pienso en tu mirá, tu mirá clavá es una bala en el (pienso en tu mirá, pienso en tu mirá)
+Pienso en tu mirá, tu mirá clavá es una bala en el pecho
+Pienso en tu mirá
+Pienso en tu mirá
+Me da miedo cuando sales
+Sonriendo pa' la calle
+Porque todos pueden ver
+Los hoyuelitos que te salen
+Me da miedo cuando sales
+Sonriendo pa' la calle
+Porque todos pueden ver
+Los hoyuelitos que te salen`
+    },
+
+
+
+    2104 : { 
+        titulo :  `DE AQUÍ NO SALES (Cap.4: Disputa)`,
+        artista : `Rosalía`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Yo que tanto te camelo
+Y tú me das pie
+Haciendo que tú de aquí no sales
+Mucho más a mí me duele
+De lo que a ti te está doliendo
+Conmigo no te equivoques
+Con el revés de la mano
+Yo te lo dejo bien claro
+Amargas penas te vendo
+Caramelos también tengo`
+    },
+
+    2105 : {
+        titulo : `RENIEGO (Cap.5: Lamento)`,
+        artista : `Rosalia`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Tiri, tiri, tiri, ay
+Ay, ay ya, ya ya ya ay
+Ay, yo río por fuera
+Y lloro por dentro
+Yo río por fuera
+Y lloro por dentro
+Y por más ducas
+Por más duquelas
+De esta pena mía
+Ay, yo no encuentro
+Yo río por fuera
+Y lloro por dentro
+Como el reniego
+Como el reniego
+Oh, reniego
+Reniego de mi sino
+Como el reniego
+Como el reniego
+Oh, ay, ay, de la horita que te he conocido`,
+    },
+
+    2106 : {
+        titulo : `PRESO (Cap.6: Clausura)`,
+        artista : `Rosalia`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Bueno, yo por amor, uff
+Bueno, hasta bajé al infierno
+Eso sí, como subí con dos ángeles
+Pues, no me arrepiento de haber bajado
+Pero bajar, bajé, ¡Eh!
+Bajar, bajé
+(Duele, duele, duele, duele)
+Te atrapa sin que te des cuenta
+Te das cuenta cuando sales
+Piensas, ¿Cómo he llegado hasta aquí?`,
+    },
+
+    2107 : {
+        titulo : `BAGDAD (Cap.7: Liturgia)`,
+        artista : `Rosalia`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Y se va a quemar si sigue ahí
+Las llamas van al cielo a morir
+Ya no hay nadie más por ahí
+No hay nadie más, sentaíta dando palmas
+Y se va a quemar si sigue ahí
+Las llamas van al cielo a morir
+Ya no hay nadie más por ahí
+No hay nadie más, no hay nadie más
+Por la noche, la salía del Bagdad
+Pelo negro, ojo' oscuro'
+Bonita, pero apena'
+Sentaíta, cabizbaja dando palma'
+Mientras a su alrededor
+Pasaban, la miraban
+La miraban sin ver na
+Solita en el infierno
+En el infierno está atrapá
+Sentaíta, la' mano' la' juntaba
+Que al compás, por bulería'
+Parecía que rezaba
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+De la' luce' sale un ángel que cayó
+Tiene una marca en el alma
+Pero ella no se la vio
+Sentaíta, al cielo quie rezarle
+Prendaíta de eso' males
+Que Dios tendrá que cobrarle
+Y se va a quemar si sigue ahí
+Las llamas van al cielo a morir
+Ya no hay nadie más por ahí
+No hay nadie más, sentaíta dando palmas
+Y se va a quemar si sigue ahí
+Las llamas van al cielo a morir
+Ya no hay nadie más por ahí
+No hay nadie más, no hay nadie más
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa
+Junta la' palma' y las separa`,
+    },
+
+    2108 : {
+        titulo : `DI MI NOMBRE (Cap.8: Éxtasis)`,
+        artista : `Rosalia`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Ali, Ali, Ali, Ali, Ali, Ali, Ali, Ali
+Ali, Ali, Ali, Ali, Ali, Ali, Ali
+Di mi nombre
+Cuando no haya nadie cerca
+Cuando no haya nadie cerca
+Cuando no haya nadie cerca
+Que las cosas
+Que las cosas que me dices
+Que las cosas que hoy me dices
+No salgan por esa puerta
+Y átame con tu cabello
+A la esquina de tu cama
+Que aunque el cabello se rompa
+Haré ver que estoy ata'a
+Que aunque el cabello se rompa
+Haré ver que estoy ata'a (ata'a)
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Di mi nombre
+Pon tu cuerpo contra el mío
+Y haz que lo malo sea bueno
+Impuro lo bendeci'o
+Y hazme rezar sobre tu cuerpo
+Y en la esquina de tu cama
+Y en el último momento
+Dime mi nombre a la cara
+Y en el último momento
+Dime mi nombre a la cara
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya
+Ay, Ali, Ali, Ali, Ali, Ali, Ali, Yali ya`,
+    },
+
+    2109 : {
+        titulo : `NANA (Cap.9: Concepción)`,
+        artista : `Rosalia`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Nana, nana, nana
+Nana, nana, nana
+En la puerta del cielo venden zapatos
+Pa' los angelitos que están descalzos
+Nadie a ti te ha conta'o que ningún sueño
+Sabe de horas o tiempos, ni tiene dueño
+Y cae la lluvia triste para mirarte
+Detrás de cada gota te mira un ángel
+Nana, nana, nana
+(Nana, nana)
+Nana`,
+    },
+
+        2110 : {
+        titulo : `MALDICIÓN (Cap.10: Cordura)`,
+        artista : `Rosalia`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+Me han dicho que no hay sali'a
+Por esta calle que voy
+Me han dicho que no hay sali'a
+Yo la tengo que encontrar
+Aunque me cueste la vida
+O aunque tengas que matar
+Ay, el querer
+Que en un momento quisiera
+Estar loca y no querer
+Porque el querer causa pena
+Pena que no tiene fin
+Si el loco vive sin ella
+No le temas al camino
+Es como una maldición
+No le temas al camino
+Si la alumbro, la confirmo
+Ya lo sabes, es lo que pasa
+Y ninguno quie'e decirlo
+He deja'o un reguero
+De sangre por el suelo
+He deja'o un reguero
+Que me lleva al primer día
+Que te dije que te quiero
+Pa' saber lo que decías
+Ay, el querer
+Que en un momento quisiera
+Estar loca y no querer
+Porque el querer causa pena
+Pena que no tiene fin
+Si el loco vive sin ella`,
+    },
+
+            2111 : {
+        titulo : ``,
+        artista : `Rosalia`,
+        album : `El Mal Querer`,
+        year : `2018`,
+        letra : `
+A ningún hombre consiento
+Que dicte mi sentencia
+Sólo Dios puede juzgarme
+Sólo a él debo obediencia
+Hasta que fuiste carcelero
+Yo era tuya compañero
+Hasta que fuiste carcelero
+Voy a tatuarme en la piel
+Tu inicial porque es la mía
+Pa' acordarme para siempre
+De lo que me hiciste un día
+De lo que me hiciste un día
+Voy a tatuarme en la piel
+Tu inicial porque es la mía
+Para acordarme para siempre
+Y recordarlo to'a la vi'a
+De lo que me hiciste un día
+De lo que me hiciste un día`,
+    }
+}
