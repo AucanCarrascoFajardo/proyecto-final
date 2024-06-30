43c6df5e0f66afd6db098880dae2ad6a7d7d1c0f
AucanFurioso <maximrosescout@gmail.com>
2024-06-30 20:40:44 -0300

    a dormir

--- Pagina Album/anclarcanciones.js
+++ Pagina Album/anclarcanciones.js
@@ -10,16 +10,27 @@ const albumId = getParameterByName('id'); // toma el parámetro 'id' de la URL
 if (albumId && listacanciones[albumId]) {
     const album = listacanciones[albumId];
     const tracklistElement = document.querySelector('.tracklist');
+    const albumInfoElement = document.querySelector('.album-info');
+    const albumCoverElement = document.querySelector('.album-cover');
+
+    // Cargar información del álbum en el header
+    albumCoverElement.src = album.info.cover;
+    albumInfoElement.innerHTML = `
+        <p>Album</p>
+        <h2>${album.info.title}</h2>
+        <p>${album.info.artist}</p>
+        <p>Lanzado el ${album.info.release_date}</p>
+        <p>${album.info.description}</p>
+    `;
 
     // Crear elementos para cada canción del álbum
-    album.forEach(cancion => {
+    album.songs.forEach(cancion => {
         const li = document.createElement('li');
         const a = document.createElement('a');
         a.textContent = cancion.titulo;
         a.href = cancion.link;
         li.appendChild(a);
         tracklistElement.appendChild(li);
-        
     });
 } else {
     const albumInfo = document.querySelector('.album-info');
--- Pagina Album/basealbumes.js
+++ Pagina Album/basealbumes.js
@@ -1,20 +1,36 @@
 const listacanciones = {
-    1 : [
-    
-    {titulo: `1. MALAMENTE (Cap.1: Augurio)`, link: `letras/index.html?id=2101`},
-    {titulo: `2. QUE NO SALGA LA LUNA (Cap.2: Boda)`, link: `letras/index.html?id=2102`},
-    {titulo: `3. PIENSO EN TU MIRÁ (Cap.3: Celos)`, link: `letras/index.html?id=2103`},
-    {titulo: `4. DE AQUÍ NO SALES (Cap.4: Disputa) `, link: `letras/index.html?id=2104`},
-    {titulo: `5. RENIEGO (Cap.5: Lamento) `, link: `letras/index.html?id=2105`},
-    {titulo: `6. PRESO (Cap.6: Clausura)`, link: `letras/index.html?id=2106`},
-    {titulo: `7. BAGDAD (Cap.7: Liturgia)`, link: `letras/index.html?id=2107`},
-    {titulo: `8. DI MI NOMBRE (Cap.8: Éxtasis)`, link: `letras/index.html?id=2108`},
-    {titulo: `9. NANA (Cap.9: Concepción)`, link: `letras/index.html?id=2109`},
-    {titulo: `10. MALDICIÓN (Cap.10: Cordura)`, link: `letras/index.html?id=2110`},
-    {titulo: `11. A NINGÚN HOMBRE (Cap.11: Poder)`, link: `letras/index.html?id=2111`}
-],
+    1 : {
+        info: {
+            cover: '../portadas/rosalia.jpg',
+            title: 'El Mal Querer',
+            artist: 'Rosalia',
+            release_date: '2 de noviembre, 2018',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac ligula nec libero cursus egestas non nec lacus.'
+        },
+        songs: [
+            {titulo: `1. MALAMENTE (Cap.1: Augurio)`, link: `letras/index.html?id=2101`},
+            {titulo: `2. QUE NO SALGA LA LUNA (Cap.2: Boda)`, link: `letras/index.html?id=2102`},
+            {titulo: `3. PIENSO EN TU MIRÁ (Cap.3: Celos)`, link: `letras/index.html?id=2103`},
+            {titulo: `4. DE AQUÍ NO SALES (Cap.4: Disputa) `, link: `letras/index.html?id=2104`},
+            {titulo: `5. RENIEGO (Cap.5: Lamento) `, link: `letras/index.html?id=2105`},
+            {titulo: `6. PRESO (Cap.6: Clausura)`, link: `letras/index.html?id=2106`},
+            {titulo: `7. BAGDAD (Cap.7: Liturgia)`, link: `letras/index.html?id=2107`},
+            {titulo: `8. DI MI NOMBRE (Cap.8: Éxtasis)`, link: `letras/index.html?id=2108`},
+            {titulo: `9. NANA (Cap.9: Concepción)`, link: `letras/index.html?id=2109`},
+            {titulo: `10. MALDICIÓN (Cap.10: Cordura)`, link: `letras/index.html?id=2110`},
+            {titulo: `11. A NINGÚN HOMBRE (Cap.11: Poder)`, link: `letras/index.html?id=2111`}
+        ]
+},
 
-    2 : [
+    2 : {
+        info: {
+            cover: '../portadas/Igor.jpg',
+            title: 'IGOR',
+            artist: 'Tyler, The Creator',
+            release_date: '2019',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac ligula nec libero cursus egestas non nec lacus.'
+        },
+        songs: [
         { titulo: "1. Igor's Theme", link: "letras/index.html?id=3101" },
         { titulo: "2.Earfquake", link: "letras/index.html?id=3102" },
         { titulo: "3. I Think", link: "letras/index.html?id=3103" },
@@ -27,5 +43,6 @@ const listacanciones = {
         { titulo: "10. Gone, Gone / Thank You", link: "letras/index.html?id=3110" },
         { titulo: "11. I Don't Love You Anymore", link: "letras/index.html?id=3111" },
         { titulo: "12. Are We Still Friends?", link: "letras/index.html?id=3112" }
-    ]
+        ]
+    }
 }
